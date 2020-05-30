0087876b26f485168014df05e89b1ddcd5c7d206
miguelmartinez624 <miguelesalador@gmail.com>
2020-05-29 23:12:10 -0400

    remove pass from code

--- communication/cmd/main.go
+++ communication/cmd/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"piwi-backend-clean/communication/core/dispatcher/email"
 )
 
 func main() {
+
+	emailPassFlag := flag.String("EMAIL_PASS", "", "password")
+
+	flag.Parse()
+	pass := *emailPassFlag
+
 	emailDispatcher := email.BuildSMTPDispatcher(
 		"devcommunicationgo@gmail.com",
-		"Mierda624.",
+		pass,
 		"smtp.gmail.com", "587")
 
 	msg := email.Message{
-		Recipient: []string{"miguelesalador@gmail.com"},
-		Body:      "Pappu sos la ostia",
+		Recipient: []string{"miguelesalador@gmail.com", "manuelclimb24@gmail.com", "jejoalca14@gmail.com"},
+		Body:      "SOMOS EL PUTO AMAZON TIO!!!",
 	}
 
 	ok, err := emailDispatcher.SendEmail(&msg)
