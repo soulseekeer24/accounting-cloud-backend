7fc9e947dbcab0e705cc3e97adb6cde787055a00
manuel <manuelclimb24@gmail.com>
2020-05-30 11:04:59 -0300

    refactor example

--- ui/accounting-ui/src/routes/index.tsx
+++ ui/accounting-ui/src/routes/index.tsx
@@ -1,28 +1,15 @@
 import React from 'react'
 import history from '../history'
 import Login from '../pages/Auth'
-import { Switch, Router, Route } from 'react-router-dom'
-import { Dashboard } from '../pages';
+import {Switch, Router, Route} from 'react-router-dom'
+import {Dashboard} from '../pages';
 
-class MainRoutes extends React.Component {
-
-    render() {
-        
-        return (
-
-            <Router history={history}>
-                <Switch>
-                    <Route exact path="/" component={Dashboard} />
-                    <Route exact path="/login" component={Login} />
-                </Switch>
-            </Router>
-
-
-
-        );
-
-    }
-
-}
-
+const MainRoutes = () => (
+    <Router history={history}>
+        <Switch>
+            <Route exact path="/" component={Dashboard}/>
+            <Route exact path="/login" component={Login}/>
+        </Switch>
+    </Router>
+);
 export default MainRoutes;
